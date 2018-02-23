d3e5351cb162c1bb3c3f2bde243f7d78fd9669be
Prince Chopra <princec@usc.edu>
2018-02-22 23:18:27 -0800

    Camera Read Write Permission and timer images added

--- app/src/main/java/com/usc/camera/activities/MainActivity.java
+++ app/src/main/java/com/usc/camera/activities/MainActivity.java
@@ -2,7 +2,9 @@ package com.usc.camera.activities;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
 import android.hardware.Camera;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -12,6 +14,8 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.usc.camera.R;
@@ -21,6 +25,9 @@ import com.usc.camera.views.CameraPreview;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -41,6 +48,19 @@ public class MainActivity extends AppCompatActivity {
 
     volatile List<String> mPicturesList;
 
+    int timeLapsed = 0;
+    int time = 0;
+
+    Runnable r;
+
+    static final Integer WRITE_EXST = 0x3;
+    static final Integer READ_EXST = 0x4;
+
+    @BindView(R.id.text_view_camera)
+    TextView textViewCamera;
+
+    @BindView(R.id.image_view_timer)
+    ImageView imageView;
 
     @Override
     protected void onResume() {
@@ -67,11 +87,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
         mFrameLayout = (FrameLayout) findViewById(R.id.preview_layout);
         //If we have the camera feature, then ask for permission, else go to the flow without camera
         frontCameraId = CameraUtil.hasFrontCameraFeature();
+
+
         if (CameraUtil.checkCameraHardware(this) && frontCameraId != -1) {
             RequestPermissionForCamera();
+
         } else {
             Toast.makeText(this, R.string.requires_camera, Toast.LENGTH_SHORT).show();
             Handler mHandler = new Handler();
@@ -86,11 +112,62 @@ public class MainActivity extends AppCompatActivity {
 
     private void showTheCameraView() {
         // Create our Preview view and set it as the content of our activity.
+
         if (mHandler == null) {
             initializeHandler();
         }
-        mPreview = new CameraPreview(this, frontCameraId, mHandler);
+        mPreview = new CameraPreview(getApplicationContext(), frontCameraId, mHandler);
         mFrameLayout.addView(mPreview);
+
+//        final Handler handler = new Handler();
+//        r = new Runnable() {
+//            public void run() {
+//                //Update and display
+//                timeLapsed += 1;
+//                switch (timeLapsed) {
+//                    case 1:
+//                        textViewCamera.setText("Now we will take photos");
+//                        break;
+//                    case 2:
+//                        textViewCamera.setText("Get Ready");
+//                        break;
+//                    case 3:
+//
+//                        break;
+//                    case 4: handler.removeCallbacks(r);
+//                        break;
+//                }
+//                handler.postDelayed(this, 1000);
+//
+//            }
+//        };
+
+//        handler.postDelayed(r,1000);
+
+//        time = 6;
+
+//        final Handler temp = new Handler();
+//        temp.postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                time -= 1;
+//                Log.e("rrbhbhbh", time+"");
+//                switch (time){
+//                    case 0:
+//                        temp.removeCallbacks(this);
+//                        textViewCamera.setText("Click");
+//                        break;
+//                    default: textViewCamera.setText(textViewCamera.getText().toString()+time);
+//                        break;
+//                }
+//            }
+//        }, 1000);
+
+//        if (mHandler == null) {
+//            initializeHandler();
+//        }
+//        mPreview = new CameraPreview(this, frontCameraId, mHandler);
+//        mFrameLayout.addView(mPreview);
     }
 
     private void initializeHandler() {
@@ -104,10 +181,36 @@ public class MainActivity extends AppCompatActivity {
                         Message msgTakePicture = new Message();
                         msgTakePicture.what = Constants.HANDLER_TAKE_PICTURE;
                         mHandler.sendMessage(msgTakePicture);
+                        Toast.makeText(getApplicationContext(), "Taken", Toast.LENGTH_LONG).show();
                         break;
                     default:
                         break;
                 }
+            }
+        };
+
+        final Handler temp = new Handler();
+        final Runnable rTemp = new Runnable() {
+            @Override
+            public void run() {
+                time -= 1;
+                switch (time){
+                    case 5:
+                        imageView.setImageResource(R.mipmap.five);
+                        break;
+                    case 4:imageView.setImageResource(R.mipmap.four);
+                        break;
+                    case 3:
+                        imageView.setImageResource(R.mipmap.three);
+                        break;
+                    case 2:imageView.setImageResource(R.mipmap.two);
+                        break;
+                    case 1:imageView.setImageResource(R.mipmap.one);
+                        break;
+                    case 0: imageView.setImageDrawable(null);
+                        break;
+                }
+                temp.postDelayed(this, 1000);
             }
         };
 
@@ -118,23 +221,33 @@ public class MainActivity extends AppCompatActivity {
                 switch (msg.what) {
                     case Constants.HANDLER_TAKE_PICTURE:
                         //take the first photo from the list
+
                         if (mPreview != null) {
                             mPreview.startPreview();
                         }
                         if (mPicturesList.size() == 0) {
                             Toast.makeText(getApplicationContext(), "All images taken " +
                                     "successfully", Toast.LENGTH_SHORT).show();
+                            textViewCamera.setText("Done");
                             break;
                         }
+
+                        time = 6;
+
+                        temp.postDelayed(rTemp, 1000);
+
                         Handler mHandler = new Handler();
                         mHandler.postDelayed(new Runnable() {
                             @Override
                             public void run() {
                                 CameraUtil.takePicture(mPicturesList.get(0), mPreview,
                                         mResultHandler);
+//                                textViewCamera.setText(""+mPicturesList.size());
+                                temp.removeCallbacks(rTemp);
+
                             }
-                        }, 6000);
-                        break;
+                        }, 7000);
+
                 }
             }
         };
@@ -166,13 +279,33 @@ public class MainActivity extends AppCompatActivity {
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     showTheCameraView();
+                    askForPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE,WRITE_EXST);
+                    askForPermission(Manifest.permission.READ_EXTERNAL_STORAGE,READ_EXST);
                 } else {
                     Toast.makeText(this, R.string.requires_camera, Toast.LENGTH_SHORT).show();
                 }
                 break;
+
             default:
                 Log.i(LOG_TAG, "onRequestPermissionsResult : Permission not handled" + requestCode);
         }
     }
 
+    private void askForPermission(String permission, Integer requestCode) {
+        if (ContextCompat.checkSelfPermission(MainActivity.this, permission) != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, permission)) {
+
+                //This is called if user has denied the permission before
+                //In this case I am just asking the permission again
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{permission}, requestCode);
+            } else {
+
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{permission}, requestCode);
+            }
+        } else {
+            Toast.makeText(this, "" + permission + " is already granted.", Toast.LENGTH_SHORT).show();
+        }
+    }
 }
--- app/src/main/java/com/usc/camera/util/Constants.java
+++ app/src/main/java/com/usc/camera/util/Constants.java
@@ -10,4 +10,5 @@ public class Constants {
     public static final int HANDLER_TAKE_PICTURE = 1;
     public static final int HANDLER_FINISH_PREVIEW = 2;
     public static final int FULL_PICTURE_TAKEN = 3;
+    public static final String TAG = "TAG";
 }
